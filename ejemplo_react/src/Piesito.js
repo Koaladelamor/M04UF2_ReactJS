34b7a095296a8d663c44eb7a2fce5ad3c8b8e938
enti <enti@enti.enti>
2022-02-25 14:26:53 +0100

    Titulito y Piesito

---
+++ ejemplo_react/src/Piesito.js
@@ -0,0 +1,11 @@
+import './Piesito.css'
+
+function Piesito(){
+	return(
+		<footer>
+			<p>Copy(c)2022 La Mamá de Piesito</p>
+		</footer>
+	);
+}
+
+export default Piesito;
---
+++ ejemplo_react/src/Titulito.js
@@ -0,0 +1,14 @@
+import Piesito from './Piesito';
+
+function Titulito(){
+	return (
+		<div id="titulo">
+		<h1>e k pacha</h1>
+		<p><strong><em>El mejor título de todos </em></strong></p>
+		<Piesito />
+		</div>
+
+	);	
+}
+
+export default Titulito;
