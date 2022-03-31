b269dfcaff9635a740f9a5cc1d14847b5031b914
enti <enti@enti.enti>
2022-03-31 23:16:26 +0200

    Ejercicio acabado

--- todolist/src/RemoveTask.js
+++ todolist/src/RemoveTask.js
@@ -3,27 +3,49 @@ import DeleteIcon from '@mui/icons-material/Delete';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogTitle from '@mui/material/DialogTitle';
-import * as React from 'react';
+import React from 'react';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 
-function RemoveTask(props){
-  	const [open, setOpen] = React.useState(false);
-
-    const handleClickOpen = () => {
-		setOpen(true);
+class RemoveTask extends React.Component{
+	
+	constructor(props){
+		super(props);
+		this.state = {
+			open: false
 		};
+	}
 
-	const handleClose = () => {
-		setOpen(false);
-		};
+    handleClickOpen = () => {
+		this.setState({
+			open: true
+		});
+	};
+
+	handleClose = () => {
+		this.setState({
+			open: false
+		});
+	};
+
+	handleDelete = () =>{
+		this.props.removeTask(this.props.id_task);
+		this.setState({
+			open: false
+		});
+	}
+
+	render(){
 
 	return(
+	<div>
 		<Button startIcon={<DeleteIcon />} variant="contained" 
-		onClick={handleClickOpen}> 
+		onClick={this.handleClickOpen}> 
 			DELETE 		
 		</Button>
-		<Dialog open={open} onClose={handleClose} aria-labelledby="alert-dialog-title">
+		<Dialog open={this.state.open} 
+		onClose={this.handleClose} 
+		aria-labelledby="alert-dialog-title">
 			<DialogTitle id="alert-dialog-title">
        			{"Delete Task?"}
        	 	</DialogTitle>
@@ -33,13 +55,17 @@ function RemoveTask(props){
 			</DialogContentText>
 			</DialogContent>
 			<DialogActions>
-          		<Button onClick={handleClose} Cancel </Button>
-         		<Button onClick={function(even){props.removeTask(props.id_task);}} autoFocus>
+          		<Button onClick={this.handleClose} >
+					Cancel 
+				</Button>
+         		<Button onClick={this.handleDelete} >
          			Delete
          		</Button>
         	</DialogActions>
 		</Dialog>			
+	</div>
 	);
+	}
 }
 
 export default RemoveTask;
